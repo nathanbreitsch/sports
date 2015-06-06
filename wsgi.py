2f3d6b26a93c75cb593d507fdadce220b90ba7d0
Nathan Breitsch <nathanbreitsch@gmail.com>
2015-06-06 19:00:49 -0400

    build db

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 import sys
+import os
 sys.path.append('sports')
 from sports import app
+
+if not os.path.isfile("./sports/sports.db"):
+    os.system("./sports/database-setup.py")
+    os.system("./sports/import_data.py")
 
 if __name__ == "__main__":
     app.run()
